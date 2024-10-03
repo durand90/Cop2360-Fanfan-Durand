6786a18addb80b0d8d4f1b15c6648686205f66bc
Fanfan <durand90@live.com>
2024-10-03 14:44:57 -0400

    This

---
+++ app/Program.cs
@@ -0,0 +1,28 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// int x = 12 * 30;                  // Statement 1
+// System.Console.WriteLine (x);     // Statement 2
+
+
+// using System;
+
+// Console.WriteLine (FeetToInches (30));
+// Console.WriteLine (FeetToInches (100));
+
+// int FeetToInches (int feet)
+// {
+//     int inches = feet * 12;
+//     return inches;
+// }
+
+
+
+
+using System;
+SayHello();
+
+void SayHello()
+{
+    Console.WriteLine ("Hello, World and now");
+}
