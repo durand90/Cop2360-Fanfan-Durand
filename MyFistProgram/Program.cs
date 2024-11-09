38ec2a7afe6f70516cd7dad7f995c9f3fc427cc6
Fanfan <durand90@live.com>
2024-11-09 13:50:06 -0500

    about

--- MyFistProgram/Program.cs
+++ MyFistProgram/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+// Console.WriteLine("This is awesome");
+
+Dictionary<int, string> rookieOfTheYear = new();
+
+rookieOfTheYear.Add(2000, "Mike Miller");
+rookieOfTheYear.Add(2001, "Jane Doe");
+rookieOfTheYear.Add(2002, "Jane Doe");
+rookieOfTheYear.Add(2003, "John Smith");
+
+Console.WriteLine(rookieOfTheYear[2002]);
