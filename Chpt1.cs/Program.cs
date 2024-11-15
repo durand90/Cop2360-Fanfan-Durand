8af99b6e82ec09f739ebebc135aa77bc3f61ffd4
Fanfan <durand90@live.com>
2024-11-14 19:45:05 -0500

    ok

--- Chpt1.cs/Program.cs
+++ Chpt1.cs/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// class Person (string firstName, string lastName)
+// {
+//    public string FirstName { get; set; } = firstName;
+//    public string LastName { get; set; } = lastName;
+// }
+
+
+//
+// void Print (string message = "") => Console.WriteLine (message);
+
+// var print = (string message = "") => Console.WriteLine (message);
+
+// print ("Marshmellow")
+// print();
